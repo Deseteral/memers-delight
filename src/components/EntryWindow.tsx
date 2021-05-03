f10b04276a63b0ce394765792bcb7387d8cb27ad
Deseteral <deseteral@gmail.com>
2021-05-03 23:50:17 +0200

    Escape hides window

--- src/components/EntryWindow.tsx
+++ src/components/EntryWindow.tsx
@@ -1,3 +1,4 @@
+import { ipcRenderer } from 'electron';
 import * as React from 'react';
 import styled from 'styled-components';
 import MemeListService from '../domain/meme-list-service';
@@ -21,7 +22,7 @@ function EntryWindow(props: EntryWindowProps): JSX.Element {
   }, []);
 
   const [entryValue, setEntryValue] = React.useState<string>('');
-  const [selectedIndex, setSelectedIndex] = React.useState<number>(0);
+  const [selectedIndex, setSelectedIndex] = React.useState<number>(0); // TODO: Move to context
   const { list, itemCount } = MemeListService.getForQuery(entryValue);
 
   const handleEntryChange = (value: string) => {
@@ -41,6 +42,11 @@ function EntryWindow(props: EntryWindowProps): JSX.Element {
         break;
       case 'Enter':
         ActionService.executeActionFor(MemeListService.getItemForIndex(list, selectedIndex));
+        setEntryValue('');
+        setSelectedIndex(0);
+        return;
+      case 'Escape':
+        ipcRenderer.send('hide-entry-window');
         setEntryValue('');
         setSelectedIndex(0);
         return;
